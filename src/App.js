58c5f2f3d9553dcc1a91b60229f1b2b85f423b3d
rpatel12438 <rpatel12438@gmail.com>
2025-09-02 18:46:34 -0400

    First commit for React project using boilerplate code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My first React projct!
+          My first React project!
         </p>
         <a
           className="App-link"
