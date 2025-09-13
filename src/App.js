3fb5d8e969ce1ce86d5122b4b63f33ef7c52fa47
rpatel12438 <rpatel12438@gmail.com>
2025-09-13 18:08:53 -0400

    Added an anchor with a link that leads to Google

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <p>
               Created By Roshan Patel
           </p>
-          <p>See more <a href="https://google.com">cat photos</a> in our gallery.</p>
+          <p>Click <a href="https://google.com">this</a> to go to Google.</p>
           <MyButton/>
       </header>
     </div>
