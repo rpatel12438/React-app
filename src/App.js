8108d0e437061544a2331e290186cc66c4ab5b8f
rpatel12438 <rpatel12438@gmail.com>
2025-10-06 12:43:14 -0400

    Cleaned up code

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ function App() {
       <header className="App-header">
           <h1>Welcome to my first webpage</h1>
           <h2>Created a second header!</h2>
-        <p>
-          My first React code!
-        </p>
-          <p>
-              Created by: Roshan Patel
-          </p>
+          <p>My first React code!</p>
+          <p>Created by: Roshan Patel</p>
           <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQGhxU4o5pOUy15ZwR4HXxqqZqVWWBqJTZxRCZb0ZK1xoGJwlIpQoCmlP3uPY2DWV6IfcgOz278y2FTLrk3gPGW7SRrgcH0VfonDHXGA_E" alt="Picture of EST GEE"/>
           <p>Click <a href="https://images.google.com">this</a> to go to Google Images.</p>
           <MyButton/>
